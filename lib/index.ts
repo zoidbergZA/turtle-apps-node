8fa70e6458b1c032258d3e3fbf0e6a82020123a0
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2019-12-23 13:43:51 +0100

    added type exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
 export { TrtlApp } from './TrtlApp';
+export { InitOptions, AppUser, Deposit, Withdrawal, UserTransfer, Recipient } from './Types';
 export { ServiceError, ServiceErrorCode } from './ServiceError';
